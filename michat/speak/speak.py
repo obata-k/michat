b0517d65befee3a7965e0c9077454e85f54f4ad4
obata-k <obata-k@kadokawa.jp>
2023-05-04 10:42:05 +0900

    fix speak moduel : environ file path and to be importable

--- michat/speak/speak.py
+++ michat/speak/speak.py
@@ -15,7 +15,7 @@ acceleration_mode = AccelerationMode.AUTO
 class ChatGPT:
     def __init__(self, max_token_size):
         self.__max_token_size = max_token_size
-        env_root = Path(os.path.dirname(__file__)).parent
+        env_root = Path(os.path.dirname(__file__)).parent.parent
         dotenv_path = os.path.join(env_root, ".env")
         load_dotenv(dotenv_path)
         openai.api_key = os.environ.get("OPENAI_API_KEY")
