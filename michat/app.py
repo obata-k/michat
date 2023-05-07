341c49c8a3327351ce06c65eddc4b06e5fb834ef
obata-k <obata-k@kadokawa.jp>
2023-05-07 22:32:33 +0900

    fix config

--- michat/app.py
+++ michat/app.py
@@ -22,9 +22,9 @@ BOT_MESSAGES = "bot_messages"
 USR_MESSAGES = "usr_messages"
 READ_INDEX = "read_index"
 SPEAKER_ID = "speaker_id"
-MODE = "chat_mode"
+MODE_INDEX = "mode_index"
 EMOTIONS = "emotions"
-FEATURE = "feature"
+FEATURE_INDEX = "feature_index"
 HISTORY = "history"
 VISIBILITY = "visibility"
 
@@ -44,10 +44,10 @@ class WebRTCRecorder:
             st.session_state[EMOTIONS] = None
         if SPEAKER_ID not in st.session_state:
             st.session_state[SPEAKER_ID] = 3
-        if MODE not in st.session_state:
-            st.session_state[MODE] = "chat"  # 'chat' or 'image'
-        if FEATURE not in st.session_state:
-            st.session_state[FEATURE] = ChatGPTFeature.ZUNDAMON
+        if MODE_INDEX not in st.session_state:
+            st.session_state[MODE_INDEX] = 0
+        if FEATURE_INDEX not in st.session_state:
+            st.session_state[FEATURE_INDEX] = 0
         if HISTORY not in st.session_state:
             st.session_state[HISTORY] = []
         if READ_INDEX not in st.session_state:
@@ -75,12 +75,9 @@ class WebRTCRecorder:
         self.status_box = st.empty()
 
         while True:
-            print("listen")
             if self.webrtc_ctx.audio_receiver:
                 try:
-                    audio_frames = self.webrtc_ctx.audio_receiver.get_frames(
-                        timeout=0.5
-                    )
+                    audio_frames = self.webrtc_ctx.audio_receiver.get_frames(timeout=1)
                 except queue.Empty:
                     self.status_box.warning("No frame arrived.")
                     continue
@@ -137,19 +134,19 @@ class WebRTCRecorder:
             st.session_state[AUDIO_BUFFER] = pydub.AudioSegment.empty()
             return text
 
-    def generate_and_play(self):
+    def generate_and_play(self, speaker_id, feature):
         if self.webrtc_ctx.state.playing or len(st.session_state[USR_MESSAGES]) == 0:
             return (None, None)
         if st.session_state[READ_INDEX] == len(st.session_state[BOT_MESSAGES]):
             return (st.session_state[BOT_MESSAGES][-1], st.session_state[EMOTIONS])
         text = st.session_state[USR_MESSAGES][-1]
         chat = ChatGPTWithEmotion(self.max_token_size)
-        speaker = Audio(self.speaker_id)
+        speaker = Audio(speaker_id)
         history = st.session_state[HISTORY][-6:]  # 最新6件
 
         try:
             st.session_state.disabled = True
-            system = system_text(self.feature)
+            system = system_text(feature)
             generated, history, emotions = chat.generate(system, text, history)
             speaker.transform(generated)
             speaker.save_wav(OUTPUT)
@@ -238,29 +235,35 @@ def voice_options():
         label_visibility=st.session_state.visibility,
         disabled=st.session_state.disabled,
     )
-    st.session_state[SPEAKER_ID] = speakers[option]
+    if st.session_state != speakers[option]:
+        st.session_state[SPEAKER_ID] = speakers[option]
     return st.session_state[SPEAKER_ID]
 
 
 def mode_options():
-    st.session_state[MODE] = st.radio(
+    modes = ["image", "chat"]
+    mode = st.radio(
         "モード選択",
-        ("image", "chat"),
+        modes,
+        index=st.session_state[MODE_INDEX],
         label_visibility=st.session_state.visibility,
         disabled=st.session_state.disabled,
     )
-    return st.session_state[MODE]
+    st.session_state[MODE_INDEX] = modes.index(mode)
+    return mode
 
 
 def feautre_option():
     feature = st.selectbox(
         "口調",
         (ChatGPTFeature.get_values()),
+        index=st.session_state[FEATURE_INDEX],
         label_visibility=st.session_state.visibility,
         disabled=st.session_state.disabled,
     )
-    st.session_state[FEATURE] = ChatGPTFeature.value_of(feature)
-    return st.session_state[FEATURE]
+    st.session_state[FEATURE_INDEX] = ChatGPTFeature.index(feature)
+    feature = ChatGPTFeature.value_of(feature)
+    return feature
 
 
 def app():
@@ -275,16 +278,14 @@ def app():
 
     with st.sidebar:
         mode = mode_options()
-        voice_options()
-        feautre_option()
+        speaker_id = voice_options()
+        feature = feautre_option()
 
+    print(st.session_state)
     webrtc.listen()  # busy loop here
     webrtc.to_text()
-    generated, emotions = webrtc.generate_and_play()
+    generated, emotions = webrtc.generate_and_play(speaker_id, feature)
     print("generated", generated)
-    print(READ_INDEX, st.session_state[READ_INDEX])
-    print(USR_MESSAGES, st.session_state[USR_MESSAGES])
-    print(BOT_MESSAGES, st.session_state[BOT_MESSAGES])
 
     if mode == "chat":
         webrtc.chat_view()
